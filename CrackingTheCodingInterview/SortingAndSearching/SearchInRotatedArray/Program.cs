4e085fdd59d718d935c78018c885ab3ddafeaf08
urospenezic <uros.penezic996@gmail.com>
2020-01-08 11:31:38 +0100

    New searching problems solved

---
+++ CrackingTheCodingInterview/SortingAndSearching/SearchInRotatedArray/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace SearchInRotatedArray
+{
+    class Program
+    {
+        // Given a sorted array of n integers that has been rotated an unknown number of times, write a code to find
+        // an element in the array. You may assume that the array was originally sorted in increasing order
+        static void Main(string[] args)
+        {
+            int[] testArray = new int[]{45, 15, 20, 25, 30, 35, 40};
+            int index= Find(testArray, 0, testArray.Length-1, 25);
+            System.Console.WriteLine(index);
+        }
+
+        private static int Find(int[] testArray, int left, int right, int x)
+        {
+            int mid = (left + right) / 2;
+            if(x== testArray[mid])
+            return mid;
+            if(right<left)
+            return -1;
+
+            if(testArray[left]<testArray[mid]) // not rotated or got back to original form
+            {
+                if(x >= testArray[left] && x < testArray[mid])
+                    return Find(testArray, left, mid-1, x);
+                else
+                    return Find(testArray, mid + 1, right, x);
+            }
+            else if(testArray[mid] < testArray[left]) //right side is normally ordered
+            {
+                if(x>testArray[mid] && x<=testArray[right])
+                    return Find(testArray, mid + 1, right, x);
+                else
+                    return Find(testArray, left, mid-1, x);
+            }
+            else if(testArray[left] == testArray[mid]) // a lot of repeated ints
+            {
+                if(testArray[mid]!=testArray[right])
+                    return Find(testArray, mid+1, right,x);
+                else
+                    {
+                        int result = Find(testArray,left,mid-1,x);
+                        if(result==-1)
+                        return Find(testArray,mid+1,right,x);
+                        else
+                        return result;
+                    }
+            }
+            return -1; //unexpected error
+        }
+    }
+}
---
+++ CrackingTheCodingInterview/SortingAndSearching/SparseSearch/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace SparseSearch
+{
+    class Program
+    {
+        // given an sorted array of strings that's interspersed, write a code to find a string
+        static void Main(string[] args)
+        {
+            var testArray = new string[]{"at", " ", " "," ", "ball", " ", " ", "car"," "," ","dad"," "};
+            var x= "ball";
+            int index = Find(testArray,0,testArray.Length-1,x);
+            System.Console.WriteLine(index);
+        }
+
+        private static int Find(string[] testArray, int left, int right, string x)
+        {
+            int mid = (left+right) / 2;
+            if(left>right)
+            return -1;
+            //if mid is an empty string, find the first non-empty one:
+            if(String.IsNullOrWhiteSpace(testArray[mid]))
+            {
+                int low = mid-1;
+                int high = mid +1;
+                while (true)
+                {
+                    if(low<left && high>right)
+                    return -1;
+                    else if(high<=right && !String.IsNullOrWhiteSpace(testArray[high]))
+                    {
+                        mid = high;
+                        break;
+                    }
+                    else if(low>=left && !String.IsNullOrWhiteSpace(testArray[low]))
+                    {
+                        mid=low;
+                        break;
+                    }
+                    low++;
+                    high++;
+                }
+            }
+            // Do binary search:
+            if(testArray[mid] == x)
+            return mid;
+            else if(testArray[mid].CompareTo(x)<0) //greater (go right)
+            return Find(testArray, mid+1, right,x);
+            else
+            return Find(testArray,left, mid-1, x);
+        }
+    }
+}
